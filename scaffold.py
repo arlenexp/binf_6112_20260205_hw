c35c1ccf6439293925b7d091e8ce480dae846bc0
arlenexp <arlenepereza@outlook.com>
2026-02-05 12:46:22 -0500

    Update scaffold.py
    added text inside file created

--- scaffold.py
+++ scaffold.py
@@ -5,7 +5,7 @@ import sys
 
 def file_gen(x): 
   new_dir = Path(x)
-  new_file = new_dir / f"{x}.txt"
+  new_file = new_dir / f"{x}.py"
 
   sys.stdout.write(f"Checking to see if file or directory '{x}' exist.... \n")
   if new_file.exists() or new_dir.exists(): 
@@ -19,6 +19,15 @@ def file_gen(x):
   sys.stderr.write("Creating directory and file... \n")
   new_dir.mkdir()
   fout = open(f"{new_file}","w")
+  fout.write("#!/usr/bin/env python3 \n") 
+  fout.write("import sys \n\n") 
+  fout.write("def main():\n")
+  fout.write("  sys.stderr.write(\"This is a practice file to generate a template file!\n")\n")
+  fout.write("  sys.stdout.write(\"Enter the title of this file: \n"\n")
+  fout.write("  title=input()\n")
+  fout.write(  sys.stdout.write(f\"The title of this document is: {title}\")\n\n)
+  fout.write("if __name__ == \"__main__\":\n")
+  fout.write("  main()\n")     
   fout.close()
 
 def main():
